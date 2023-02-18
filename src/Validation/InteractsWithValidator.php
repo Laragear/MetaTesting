ed749e95a8c6155fb5897c8fc48f701e1da6babe
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-02-18 00:03:26 -0300

    Minor refactoring for validation.

--- src/Validation/InteractsWithValidator.php
+++ src/Validation/InteractsWithValidator.php
@@ -10,6 +10,23 @@ use function is_array;
 trait InteractsWithValidator
 {
     /**
+     * Runs the validation name with data and rules
+     *
+     * @internal
+     * @param  array<string, mixed>|string  $data
+     * @param  array<string, array<string, string>|string>|string  $rules
+     * @return bool
+     */
+    protected function runValidation(array|string $data, array|string $rules): bool
+    {
+        if (! is_array($data) && ! is_array($rules)) {
+            [$data, $rules] = [[$rules => $data], [$rules => $rules]];
+        }
+
+        return $this->app->make('validator')->make($data, $rules)->fails();
+    }
+
+    /**
      * Assert a given rule passes.
      *
      * @param  array<string, mixed>|string  $data
@@ -18,13 +35,7 @@ trait InteractsWithValidator
      */
     protected function assertValidationPasses(array|string $data, array|string $rules): void
     {
-        if (! is_array($data) && ! is_array($rules)) {
-            [$data, $rules] = [[$rules => $data], [$rules => $rules]];
-        }
-
-        $validator = $this->app->make('validator')->make($data, $rules);
-
-        static::assertFalse($validator->fails(), 'The rule has not passed validation.');
+        static::assertFalse($this->runValidation($data, $rules), 'The rule has not passed validation.');
     }
 
     /**
@@ -36,12 +47,6 @@ trait InteractsWithValidator
      */
     protected function assertValidationFails(array|string $data, array|string $rules): void
     {
-        if (! is_array($data) && ! is_array($rules)) {
-            [$data, $rules] = [[$rules => $data], [$rules => $rules]];
-        }
-
-        $validator = $this->app->make('validator')->make($data, $rules);
-
-        static::assertTrue($validator->fails(), 'The rule has not failed validation.');
+        static::assertTrue($this->runValidation($data, $rules), 'The rule has not failed validation.');
     }
 }
